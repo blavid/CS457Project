96c8f20669f80a3488ed415d2991d16036ee658b
robot <robot@robot-VirtualBox.(none)>
2014-02-20 19:13:09 -0800

    Minor tweaks to arrival function... instead of getting body information right away, will return the response and we can use other functions to further unpack it and error check.

--- Code/TrimetFunctions.hs
+++ Code/TrimetFunctions.hs
@@ -1,6 +1,7 @@
 import Network.HTTP
+import Network.Stream
 
 baseArrivals = "http://developer.trimet.org/ws/V1/arrivals/appID/3B5489BFA2CDF3D5711521B76/json/true/"
 
-arrivalFunction :: String -> IO String
-arrivalFunction id = simpleHTTP (getRequest (baseArrivals ++ "locIDs/" ++ id ++ "/")) >>= getResponseBody
+getArrivals    :: String -> IO (Result (Response String))
+getArrivals id = simpleHTTP (getRequest (baseArrivals ++ "locIDs/" ++ id ++ "/"))
