ce4f211844d74511709aabdc7590f968e910e927
robot <robot@robot-VirtualBox.(none)>
2014-02-26 18:57:25 -0800

    modifications to WebServer.hs.. testing templating

--- Code/WebServer.hs
+++ Code/WebServer.hs
@@ -1,10 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Monad    (msum)
-import Happstack.Server (nullConf, simpleHTTP, ok, dir, path, seeOther)
+import Happstack.Server
+import           Text.Blaze ((!))
+import qualified Text.Blaze.Html4.Strict as H
+import qualified Text.Blaze.Html4.Strict.Attributes as A
+
+appTemplate :: String -> [H.Html] -> H.Html -> H.Html
+appTemplate title headers body =
+    H.html $ do
+      H.head $ do
+        H.title (H.toHtml title)
+        H.meta ! A.httpEquiv "Content-Type"
+               ! A.content "text/html;charset=utf-8"
+        sequence_ headers
+      H.body $ do
+        body
+
+helloBlaze :: ServerPart Response
+helloBlaze =
+   ok $ toResponse $
+    appTemplate "Hello, Blaze!"
+                [H.meta ! A.name "keywords"
+                        ! A.content "happstack, blaze, html"
+                ]
+                (H.p $ do "Hello, "
+                          H.b "blaze-html!")
 
 main :: IO ()
-main = simpleHTTP nullConf $
-  msum [ dir "hello" $ path $ \s -> ok $ "Hello, " ++ s
-       , seeOther "/hello/Haskell" "/hello/Haskell"
-       ]
+main = simpleHTTP nullConf $ helloBlaze
